d9d360c334a9a56a54be5554e04071d0648f1732
chandrakantkarma <160504431+chandrakantkarma@users.noreply.github.com>
2024-03-24 13:32:52 +0000

    datatypes

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+// alert(3+3)//we are using node js, not browser
+console.log(3+
+    3);//code readabilty should be high
+console.log("shivam");
+let name="shivam"
+let age=20
+let isloggedin=true
+// number =>2 to power 53
+// bigint =>bigger than number
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined
+// symbol=>unique
+// object
+console.log(typeof "")
+console.log(typeof 3);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof true);

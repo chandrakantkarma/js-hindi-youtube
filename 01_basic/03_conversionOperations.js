9e09849750c296d6644812b08e2c62ddede952b6
chandrakantkarma <160504431+chandrakantkarma@users.noreply.github.com>
2024-03-25 07:31:03 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score="33"   //number=>33
+// let score="33kj" // NaN
+// let score=null   //number=>0
+// let score=true   // true=>1,false=>0
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+let valueInNumber=Number(score) // N always capital
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+let isLoggedIn="shivam"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);  
+
+// 1=>true; 0=> false
+// "" => false
+// "shivam" => true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

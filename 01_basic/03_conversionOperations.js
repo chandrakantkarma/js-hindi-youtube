c58517802ef267903dae5206a22df7d44596879f
chandrakantkarma <160504431+chandrakantkarma@users.noreply.github.com>
2024-03-25 08:05:02 +0000

    porations

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -18,5 +18,42 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 // "shivam" => true
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********************Operations********************
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(3%2);
+
+let str1="hello"
+let str2=" shivam"
+// let str3=str1+str2
+// console.log(str1+str2);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// if first coming string statement than all are treated as string or if first coming statement is number first they solve and then they convertd
+
+// console.log(3+4*5%3); // not recommended
+
+// console.log(+true); //not recommended
+// console.log(+"");   //not recommended
+
+let num1,num2,num3
+num1=num2=num3=2+2 // not rec
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);

5dc2578f15295163311d384e0ed1156bbeadd4db
chandrakantkarma <160504431+chandrakantkarma@users.noreply.github.com>
2024-03-26 09:35:22 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=0);
+// console.log(2!=2);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null<1);
+// console.log(null==0);
+// console.log(null>=0);
+
+// comparision < <= > >= convert null into 0 and == not convert
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+
+//  undefined is always false
+// we should avoid these type of code
+
+// === strictly check
+console.log("2"===2);

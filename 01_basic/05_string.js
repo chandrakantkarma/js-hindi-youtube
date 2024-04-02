7eb45feb4d5542e3ec8aaf305436da1f4e0730b6
chandrakantkarma <160504431+chandrakantkarma@users.noreply.github.com>
2024-04-02 02:54:37 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,32 @@
+const name="shivam"
+const repoCount=50
+// console.log(name + repoCount+ " value");
+// console.log(`Hello my name is ${name} and my  repo count is ${repoCount}`);
+
+const gameName=new String('shivam-sk')
+// console.log(gameName[0]);
+// console.log(gameName.charAt.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('v'));
+
+const newString=gameName.substring(0,6);
+// console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+// console.log(anotherString);
+
+const newStringOne="  shivam  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://shivam.com/shivam%20karma"
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('sunder'));
+// console.log(url.includes('shivam'));
+
+console.log(gameName.split('-'));

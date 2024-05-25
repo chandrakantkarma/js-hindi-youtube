3848a4760ca751b564e9befb795ad7d7a7d3df5e
chandrakantkarma <160504431+chandrakantkarma@users.noreply.github.com>
2024-05-25 07:04:11 +0000

    scopes

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -5,10 +5,31 @@ if(true){
     const b=20
     // var c = 30
     c =30 //here we changed c=300 to c=30
-    console.log("INNER: ",a);
+    // console.log("INNER: ",a);
 }
 
 // console.log(a); //error
 // console.log(b); //error
-console.log(c);    // 30
+// console.log(c);    // 30
 
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);  //error
+    two()
+}
+// one()
+
+if(true){
+    const username= "hitesh"
+    if(username === "hitesh"){
+        const website=" youtube"
+        console.log(username+website);
+    }
+        // console.log(website);
+}
+// console.log(username);

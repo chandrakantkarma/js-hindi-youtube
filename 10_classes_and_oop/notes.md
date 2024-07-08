8166c8d5e8266092a7ef95b6eae73a0709d4dac8
chandrakantkarma <160504431+chandrakantkarma@users.noreply.github.com>
2024-07-08 17:03:12 +0000

    oop

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,23 @@
+# javascript and classes
+
+## OOP
+
+##  OBJECT
+-   COLLECTION of properties and methods
+-   toLowerCase
+
+## why use OOP
+
+## parts of OOP
+Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+##  4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,42 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got user details from darabase")
+        // console.log(this.username)
+        console.log(this)
+    }
+}
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this)
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`)
+    }
+
+    // return this; //if we don't write return this then there is no effect on our output so its optional
+}
+
+// const userOne = User("hitesh",12,true);
+// const userTwo = User("chai",11,false);
+// here if we call user another time then value will be over-write by second call
+// so solution is new keyword
+
+
+const userOne = new User("hitesh",12,true);
+const userTwo = new User("chai",11,false);
+// console.log(userOne)
+// console.log(userTwo)
+
+console.log(userOne.constructor)
